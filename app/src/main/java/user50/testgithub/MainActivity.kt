1ddfd3768147c39a2da83496a906f327b8bacc4d
develop50 <develop50b@gmail.com>
2022-02-17 17:06:28 +0900

    commit test2

--- app/src/main/java/user50/testgithub/MainActivity.kt
+++ app/src/main/java/user50/testgithub/MainActivity.kt
@@ -2,10 +2,20 @@ package user50.testgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(Companion.TAG, "Change message 3")
+
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
 }
